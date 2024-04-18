ca6d8f4c3a190440a9e40a4603a1d5337c15439a
Jacob Kelly <jskelly2021@gmail.com>
2024-04-17 20:38:22 -0500

    Completed Creating and Combining Views Section

---
+++ Landmarks/Landmark.swift
@@ -0,0 +1,27 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Jacob Kelly on 4/17/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image { Image(imageName) }
+    
+    private var coordinates: Coordinates
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
